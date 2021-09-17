bff790ea51904ca52e50df9146d258d923e0b736
HopeAnnihilator <59497210+HopeAnnihilator@users.noreply.github.com>
2021-09-16 23:54:58 -0500

    sorry it needs to be more obscure :(

--- project01/project01Optimized.cpp
+++ project01/project01Optimized.cpp
@@ -6,14 +6,14 @@ void LoadImage(const string imagefile, int image[MAXROWS][MAXCOLS]) {
 
     // dump the first two lines, headers
     for (int i = 0; i < 2; i++) {
-        getline(input, data);
+        input.ignore (numeric_limits<streamsize>::max(), '\n' );
     }
 
     // read image data and save to array
-    for (int row = 0; row < MAXROWS; row++) {
+    for (int row = MAXROWS - 1; row >= 0; row--) {
         getline(input, data);
-        for (int col = 0; col < MAXCOLS; col++) {
-            image[row][col] = data[col * 2] - 48;
+        for (int col = MAXCOLS - 1; col >= 0; col--) {
+            (&image[0][0])[(MAXROWS - 1 - row) * 10 + col] = data[col * 2] - 48;
         }
     }
 
@@ -22,64 +22,65 @@ void LoadImage(const string imagefile, int image[MAXROWS][MAXCOLS]) {
 }
 
 void FlipHorizontal(int image[MAXROWS][MAXCOLS]){
-    for (int row = 0; row < MAXROWS; row++) {
-        for (int col=0; col < MAXCOLS / 2; col++) {
-            image[row][col] = image[row][col] ^ image[row][9 - col];
-            image[row][9 - col] = image[row][col] ^ image[row][9 - col];
-            image[row][col] = image[row][col] ^ image[row][9 - col];
+    for (int row = MAXROWS - 1; row >= 0; row--) {
+        for (int col = MAXCOLS / 2; col >= 0; col--) {
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[row * 10 + (MAXCOLS - 1 -col)];
+            (&image[0][0])[row * 10 + (MAXCOLS - 1 -col)] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[row * 10 + (MAXCOLS - 1 -col)];
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[row * 10 + (MAXCOLS - 1 -col)];
         }
     }
 }
 
 void FlipVertical(int image[MAXROWS][MAXCOLS]){
-    for (int row = 0; row < MAXROWS / 2; row++) {
-        for (int col=0; col < MAXCOLS; col++) {
-            image[row][col] = image[row][col] ^ image[9 - row][col];
-            image[9 - row][col] = image[row][col] ^ image[9 - row][col];
-            image[row][col] = image[row][col] ^ image[9 - row][col];
+    for (int row = MAXROWS / 2; row >= 0; row--) {
+        for (int col = MAXCOLS - 1; col >= 0; col--) {
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[(MAXROWS - 1 - row) * 10 + col];
+            (&image[0][0])[(MAXROWS - 1 - row) * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[(MAXROWS - 1 - row) * 10 + col];
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[(MAXROWS - 1 - row) * 10 + col];
         }
     }
 }
 
 
 void Transpose(int image[MAXROWS][MAXCOLS]){
-    for (int row = 0; row < MAXROWS - 1; row++) {
+    for (int row = MAXROWS - 1; row >= 0; row--) {
         for (int col = row + 1; col < MAXROWS; col++) {
-            image[row][col] = image[row][col] ^ image[col][row];
-            image[col][row] = image[row][col] ^ image[col][row];
-            image[row][col] = image[row][col] ^ image[col][row];
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ image[col][row];
+            image[col][row] = (&image[0][0])[row * 10 + col] ^ image[col][row];
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ image[col][row];
         }
     }
 }
 
 void RotateCW(int image[MAXROWS][MAXCOLS]) {
-    for (int row = 0; row < MAXROWS / 2; row++) {
-        for (int col = 0; col < MAXCOLS / 2; col++) {
-            image[row][col] = image[row][col] ^ image[row][9 - col]; 
-            image[row][9 - col] = image[row][col] ^ image[row][9 - col]; 
-            image[row][col] = image[row][col] ^ image[row][9 - col]; 
-            image[row][col] = image[row][col] ^ image[9 - row][9 - col]; 
-            image[9 - row][9 - col] = image[row][col] ^ image[9 - row][9 - col]; 
-            image[row][col] = image[row][col] ^ image[9 - row][9 - col]; 
-            image[row][col] = image[row][col] ^ image[9 - row][col]; 
-            image[9 - row][col] = image[row][col] ^ image[9 - row][col]; 
-            image[row][col] = image[row][col] ^ image[9 - row][col]; 
+    for (int row = MAXROWS / 2; row >= 0; row--) {
+        for (int col = MAXCOLS / 2; col >= 0; col--) {
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[row * 10 + (MAXCOLS - 1 -col)]; 
+            (&image[0][0])[row * 10 + (MAXCOLS - 1 -col)] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[row * 10 + (MAXCOLS - 1 -col)]; 
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[row * 10 + (MAXCOLS - 1 -col)]; 
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[(MAXROWS - 1 - row) * 10 + (MAXCOLS - 1 -col)]; 
+            (&image[0][0])[(MAXROWS - 1 - row) * 10 + (MAXCOLS - 1 -col)] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[(MAXROWS - 1 - row) * 10 + (MAXCOLS - 1 -col)]; 
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[(MAXROWS - 1 - row) * 10 + (MAXCOLS - 1 -col)]; 
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[(MAXROWS - 1 - row) * 10 + col]; 
+            (&image[0][0])[(MAXROWS - 1 - row) * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[(MAXROWS - 1 - row) * 10 + col]; 
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[(MAXROWS - 1 - row) * 10 + col]; 
         }
     }
 }
 
 void RotateCCW(int image[MAXROWS][MAXCOLS]){
-    for (int row = 0; row < MAXROWS / 2; row++) {
-        for (int col = 0; col < MAXCOLS / 2; col++) {
-            image[row][col] = image[row][col] ^ image[9 - row][col]; 
-            image[9 - row][col] = image[row][col] ^ image[9 - row][col]; 
-            image[row][col] = image[row][col] ^ image[9 - row][col]; 
-            image[row][col] = image[row][col] ^ image[9 - row][9 - col]; 
-            image[9 - row][9 - col] = image[row][col] ^ image[9 - row][9 - col]; 
-            image[row][col] = image[row][col] ^ image[9 - row][9 - col]; 
-            image[row][col] = image[row][col] ^ image[row][9 - col]; 
-            image[row][9 - col] = image[row][col] ^ image[row][9 - col]; 
-            image[row][col] = image[row][col] ^ image[row][9 - col]; 
+    for (int row = MAXROWS / 2; row >= 0; row--) {
+        for (int col = MAXCOLS / 2; col >= 0; col--) {
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[(MAXROWS - 1 - row) * 10 + col]; 
+            (&image[0][0])[(MAXROWS - 1 - row) * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[(MAXROWS - 1 - row) * 10 + col]; 
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[(MAXROWS - 1 - row) * 10 + col]; 
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[(MAXROWS - 1 - row) * 10 + (MAXCOLS - 1 -col)]; 
+            (&image[0][0])[(MAXROWS - 1 - row) * 10 + (MAXCOLS - 1 -col)] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[(MAXROWS - 1 - row) * 10 + (MAXCOLS - 1 -col)]; 
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[(MAXROWS - 1 - row) * 10 + (MAXCOLS - 1 -col)]; 
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[row * 10 + (MAXCOLS - 1 -col)]; 
+            (&image[0][0])[row * 10 + (MAXCOLS - 1 -col)] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[row * 10 + (MAXCOLS - 1 -col)]; 
+            (&image[0][0])[row * 10 + col] = (&image[0][0])[row * 10 + col] ^ (&image[0][0])[row * 10 + (MAXCOLS - 1 -col)]; 
         }
     }
 }
+
