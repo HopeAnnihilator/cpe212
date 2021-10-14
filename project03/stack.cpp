4e57975d6b42351f05bc716997a025fcc41f523b
HopeAnnihilator <59497210+HopeAnnihilator@users.noreply.github.com>
2021-10-14 00:27:32 -0500

    Add files via upload

---
+++ project03/stack.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <new>
+#include "stack.h"
+
+using namespace std;
+
+void Stack::Resize(int n) {
+    try {
+        num *= 2;
+        int* array = new int [num];
+        Push(n);
+    } catch (exception &e) {
+        StackFull full;
+        throw full;
+    }
+}
+
+Stack::Stack(int n) {
+    array = new int[n];
+    num = n;
+    top = -1;
+}
+
+Stack::~Stack() {
+    delete[] array;
+}
+
+void Stack::Push(int n) {
+    if (!IsFull()) {
+        top++;
+        array[top] = n;
+    } else {
+        Resize(n);
+    } 
+}
+
+void Stack::Pop() {
+    if (IsEmpty()) {
+        StackEmpty empty;
+        throw empty;
+    } else {
+        top--;
+    }
+}
+
+bool Stack::IsEmpty() const {
+    return (top == -1);
+}   
+
+bool Stack::IsFull() const {
+    return (top == num - 1);
+}
+
+void Stack::MakeEmpty() {
+    top = -1;
+}
+
+int Stack::Top() const {
+    if (!IsEmpty()) {
+        return array[top];
+    } else {
+        StackEmpty empty;
+        throw empty;
+    }
+}
+
+int Stack::Size() const {
+    return top + 1;
+}
+
+int Stack::Max() const {
+    if (IsEmpty()) {
+        StackEmpty empty;
+        throw empty;
+    } else {
+        int max = Peek(0);
+        for (int i = 0; i <= top; i++) {
+            if (Peek(i) > max) {
+                max = Peek(i);
+            }
+        }
+        return max;
+    }
+}
+
+int Stack::Min() const {
+    if (IsEmpty()) {
+        StackEmpty empty;
+        throw empty;
+    } else {
+        int min = Peek(0);
+        for (int i = 0; i <= top; i++) {
+            if (Peek(i) < min) {
+                min = Peek(i);
+            }
+        }
+        return min;
+    }
+}
+
+int Stack::Peek(unsigned int n) const {
+    if (n > top || IsEmpty()) {
+        StackInvalidPeek invalid;
+        throw invalid;
+    } else {
+        return array[top - n];
+    }
+}
+
+int Stack::Capacity() const {
+    return num;
+}
